bb86c1cf60c618fbd71ee6648e2492b65aff43e6
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2024-08-13 15:27:14 +0200

    Use MongoDB

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,5 @@
 import {Queue} from "docmq";
+import {MongoClient} from "mongodb";
 import {MongoDriver} from "docmq/driver/mongo";
 
-export const queue = new Queue(new MongoDriver({url: process.env.DATA_API_URL}), "docmq");
+export const queue = new Queue(new MongoDriver(new MongoClient({url: process.env.DATA_API_URL})), "docmq");
